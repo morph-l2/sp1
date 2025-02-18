defcb7235c158da81af1deb0e5ac51fc4703d95b
dongliangCai <79450676+dongliangCai@users.noreply.github.com>
2025-02-18 17:20:02 +0800

    fix memcopy name

--- crates/core/machine/src/syscall/memcpy.rs
+++ crates/core/machine/src/syscall/memcpy.rs
@@ -66,7 +66,7 @@ impl<F: PrimeField32, NumWords: ArrayLength + Send + Sync, NumBytes: ArrayLength
     type Program = Program;
 
     fn name(&self) -> String {
-        format!("MemCopy{}Chip", NumWords::USIZE)
+        format!("MemCopy{}Chip", NumBytes::USIZE)
     }
 
     fn generate_trace(&self, input: &Self::Record, output: &mut Self::Record) -> RowMajorMatrix<F> {
